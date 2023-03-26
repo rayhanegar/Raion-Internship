6f01fb6d2d3315d73727c04ae73b6c56fe3e8441
Rayhan Egar <rresnn@gmail.com>
2023-03-26 21:58:40 +0700

    March 26, Progress 1
    1. viewPager2 onboarding page
    2. themes.xml updated
    3. splash screen

---
+++ app/src/main/java/com/example/raioninternship/onboarding/ViewPagerFragment.kt
@@ -0,0 +1,38 @@
+package com.example.raioninternship.onboarding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.raioninternship.R
+import com.example.raioninternship.databinding.FragmentViewPagerBinding
+import com.example.raioninternship.onboarding.onboardingScreen.*
+
+class ViewPagerFragment : Fragment() {
+
+    private var _binding: FragmentViewPagerBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentViewPagerBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        val fragmentList = arrayListOf<Fragment>(
+            Onboarding0(), // Splash screen to be
+            Onboarding1(),
+            Onboarding2(),
+            Onboarding3(),
+            Onboarding4()
+        )
+        val adapter = ViewPagerAdapter (fragmentList, requireActivity().supportFragmentManager, lifecycle)
+        binding.viewPager.adapter = adapter
+        return view
+
+    }
+}
---
+++ app/src/main/java/com/example/raioninternship/onboarding/onboardingScreen/Onboarding0.kt
@@ -0,0 +1,21 @@
+package com.example.raioninternship.onboarding.onboardingScreen
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.raioninternship.R
+
+
+class Onboarding0 : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_onboarding0, container, false)
+    }
+
+}
