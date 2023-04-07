667c5d0bb679b4494b51a12dcd1cd1ac7a91b5f5
LYRA MUKHRODI <dillah.lyra@gmail.com>
2023-04-07 10:48:58 +0700

    On going kiat IT

---
+++ app/src/main/java/com/example/raioninternship/screen/kiat/Kiat1Screen.kt
@@ -0,0 +1,24 @@
+package com.example.raioninternship.screen.kiat
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.raioninternship.databinding.ScreenKiatIt1Binding
+
+class Kiat1Screen : Fragment() {
+
+    private lateinit var binding : ScreenKiatIt1Binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+
+
+        return view
+    }
+}
